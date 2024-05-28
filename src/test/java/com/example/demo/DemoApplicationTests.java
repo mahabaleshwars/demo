1199340c97fb1358f77c46750be1a23a18ed3ce0
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2024-05-28 16:47:09 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,17 +6,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-	// Private constructor to prevent instantiation
-        private DemoApplication() {
-           // This constructor will prevent the class from being instantiated
-           // since it's private and cannot be accessed from outside the class.
-           // Utility classes should not have a public or default constructor.
-           // By making it private, we ensure that no instance of this class can be created.
-      	  throw new AssertionError("Utility class should not be instantiated");
-  	}
-  
-	@Test
-	void contextLoads() {
-	}
+    // Private constructor to prevent instantiation
+    private DemoApplication() {
+        // This constructor will prevent the class from being instantiated
+        // since it's private and cannot be accessed from outside the class.
+        // Utility classes should not have a public or default constructor.
+        // By making it private, we ensure that no instance of this class can be created.
+        throw new AssertionError("Utility class should not be instantiated");
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
